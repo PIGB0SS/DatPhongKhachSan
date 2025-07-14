d8abb58376bc0ae8c3d8c913c25d9c20f76d5270
PIGB0SS <107063427+PIGB0SS@users.noreply.github.com>
2025-07-15 00:36:12 +0700

    phan2

---
+++ Domain/Entities/HotelNumber.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    public class HotelNumber
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Hotel_Number { get; set; }
+        [ForeignKey("Hotel")]
+        public int HotelId { get; set; }
+        public Hotel? Hotel { get; set; }
+        public string? SpecialDetails { get; set; }
+    }
+}
